f83d894c71a93eb3bae31cd4bfd32e3664dc9939
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-03-13 14:40:40 +0300

    Array Methods done

--- Array Methods/app.js
+++ Array Methods/app.js
@@ -22,6 +22,14 @@ split(bölmek)       : Stringi belirli bir ifadeye göre bölüp diziye cevirmek
 
 indexOf             : elemanın index numarasını verir. 
 includes            : verilen elemanı içeriyor mu ona bakar 
+
+map                 : ForEach ile aynı fakat tüm elemanları dönerken farklı bir dizi oluşturur
+some                : belirtilen koşul dizinin herhangi bir elemanına uyuyorsa true döner
+every               : some ile aynı fakat koşulların tüm elemanlar için aynı olması lazım
+filter              : Belirtilen koşulu sağlayan elemnalar yeni bir dizi içerisine alınır
+find                : Belirtilen koşulu sağlayan dizi elemanını bulmamızı sağlar
+sort                : Dizi içerisindeki elemanları sıralamyı sağlar. Yeni bir dizi oluşturmaz
+reduce              : Bu metot diziyi bir değere indirger
 */
 
 
@@ -55,3 +63,40 @@ console.log(telefonlar.indexOf("Huawei"))
 
 
 console.log(telefonlar.includes("Samsung"))
+
+
+let yenitelefonlar = telefonlar.map(function(isim){
+    return isim+"i"
+})
+console.log(yenitelefonlar);
+
+let sayilar = [1,5,2,3,4]
+let result = sayilar.some(function(sayi){
+    return sayi<3
+})
+console.log(result);
+
+let resultEvery = sayilar.every(function(sayi){
+    return sayi < 5
+})
+console.log(resultEvery)
+
+let resultFilter = sayilar.filter(function(sayi){
+    return sayi > 2
+})
+console.log(resultFilter)
+
+let resultFind = sayilar.find(function(sayi){
+    return sayi > 2
+})
+console.log(resultFind)
+
+sayilar.sort()
+console.log(sayilar)
+
+
+function indirgeyici(akumulator, sayi){
+    return akumulator - sayi;
+}
+let sonuc = sayilar.reduce(indirgeyici, 0);
+console.log(sonuc)
