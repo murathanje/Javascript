8cd763c7e8700f589a52731191d8174de325c9c4
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-03-20 18:55:08 +0300

    DOM done to part1

---
+++ DOM/app.js
@@ -0,0 +1,19 @@
+
+
+// let value = document.location.href;
+// let value = document.location.hostname;
+// let value = document.location.host;
+// let value = document.characterSet;
+// let value = document.title;
+// let value = document.links.item(0).id;
+// let value = document.links.item(0).getAttribute("href");
+// let value = document.links.item(0).classList[3];
+// let value = document.forms.item(1).id;
+// let value = document.forms.item(1).children;
+// let value = document.domain;
+// let value = document.contentType;
+
+
+console.log(document.location)
+
+console.log(value);
