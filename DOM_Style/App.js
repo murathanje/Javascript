e2614191e5edebf5fbb2222ad1ba833a1d68dff2
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-04-16 12:35:27 +0300

    DOM Style done

---
+++ DOM_Style/App.js
@@ -0,0 +1,12 @@
+// Style özelliklerini kullanma
+
+const todo = document.querySelector(".list-group-item")
+
+// console.log(todo);
+// console.log(todo);
+todo.style.color = "green"
+todo.style.backgroundColor = "red"
+todo.style.fontWeight = "1000"
+todo.style.paddingTop = "100px"
+todo.style.paddingLeft = "100px"
+
