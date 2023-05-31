6fe0ee60ade056d386179193cb7ff9fa972cb5e7
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-05-31 22:57:39 +0300

    Arrow Function was done

---
+++ ES6/App.js
@@ -0,0 +1,9 @@
+// Arrow function
+
+const yazdir = (isim) => {
+    console.log("Merhaba",isim);
+}
+
+// const yazdir = sayi => sayi + 1; Alternatif kısa kullanım tek işlemlerde : parantez, parametre,retrun kalkar
+
+yazdir("Murathan");
