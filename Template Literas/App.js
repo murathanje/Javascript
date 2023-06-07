12fc0c85362221a687ba7c5ad11dddff43e2eeae
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-06-07 21:57:03 +0300

    Template Literate was done

---
+++ Template Literas/App.js
@@ -0,0 +1,12 @@
+//      Template Literas
+
+
+const yazdir = (firstName, lastName) => 
+// console.log("İsim:" + " " + firstName + " " + "Soyad" + " " +  lastName );
+console.log(`
+
+İsim: ${firstName} Soyad: ${lastName}
+
+`);
+
+yazdir("Murathan", "Bayram");
